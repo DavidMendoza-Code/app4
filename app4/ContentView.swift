b2d91df230ba9a29587c3805398319fee11b46bc
davidmendoza <johnda21-@hotmail.com>
2021-02-02 19:20:14 +0100

    commit

--- app4/ContentView.swift
+++ app4/ContentView.swift
@@ -9,13 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+       modalView()
+            .font(.callout)
+            .foregroundColor(Color.red)
             .padding()
+            
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+          
+        }
     }
 }
---
+++ app4/modalView.swift
@@ -0,0 +1,20 @@
+//
+//  modalView.swift
+//  app4
+//
+//  Created by Desarrollador on 2/2/21.
+//
+
+import SwiftUI
+
+struct modalView: View {
+    var body: some View {
+        Text("Modal View")
+    }
+}
+
+struct modalView_Previews: PreviewProvider {
+    static var previews: some View {
+        modalView()
+    }
+}
