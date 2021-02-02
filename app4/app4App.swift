14f38869e2c72d79e20efd657f55fec1995ef159
davidmendoza <johnda21-@hotmail.com>
2021-02-02 15:41:35 +0100

    Initial Commit

---
+++ app4/app4App.swift
@@ -0,0 +1,17 @@
+//
+//  app4App.swift
+//  app4
+//
+//  Created by Desarrollador on 2/2/21.
+//
+
+import SwiftUI
+
+@main
+struct app4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
